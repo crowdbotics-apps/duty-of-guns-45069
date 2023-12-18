88171eab43162d5555e6f625a39c55a0d2a1b4bc
Crowdbotics <team@crowdbotics.com>
2023-12-18 01:29:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "duty-of-guns-45069",
+  url: "https://duty-of-guns-45069.botics.co",
+  api: "https://duty-of-guns-45069.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
